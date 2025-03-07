1798c208d3b4d6b6ca625c30b793c6496780336e
fady <fady41487@gmail.com>
2025-03-07 20:07:45 +0200

    Add project files.

---
+++ TechXpress.Domain/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechXpress.Domain.Models
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+
+      public ICollection<inventory> Inventories { get; set; }
+    }
+}
