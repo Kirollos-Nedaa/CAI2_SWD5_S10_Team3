3f0d32f6a29cd5f7ad5078619e8c60fb6437632e
Abanoub Saber <abanoubsaber87@gmail.com>
2025-05-01 01:00:38 +0300

    add cart

--- TechXpress.Domain/Models/Cart.cs
+++ TechXpress.Domain/Models/Cart.cs
@@ -8,6 +8,9 @@ namespace TechXpress.Domain.Models
 {
     public class Cart
     {
-
+        public int Cart_ID { get; set; }
+        public int Customer_ID { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
     }
 }
---
+++ TechXpress.Infrastructure/Config/CartConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechXpress.Domain.Models;
+
+namespace TechXpress.Infrastructure.Config
+{
+    public class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.HasKey(c => c.Cart_ID);
+
+            builder.Property(c => c.Quantity)
+                   .IsRequired();
+
+            builder.Property(c => c.Total)
+                   .IsRequired()
+                   .HasColumnType("decimal(18,2)");
+
+            builder.HasOne<Customer>()  
+                   .WithMany()
+                   .HasForeignKey(c => c.Customer_ID) // check if one to one or one to many
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
