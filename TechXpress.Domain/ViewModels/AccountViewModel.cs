6d2f09bfdc47f28a25f0ff78000ecac22a338e44
Fady10emad <fady41487@gmail.com>
2025-05-13 03:29:17 +0300

    Making Auth for user login register reset password

---
+++ TechXpress.Domain/ViewModels/AccountViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechXpress.Domain.ViewModels
+{
+    public class AccountViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
